f548cdbe85c6bd43fcd0e82e74cbc32ca2f91398
Paul Tegnazian <paultegnazian@pauls-mbp.myfiosgateway.com>
2020-09-19 14:34:19 -0400

    Cleaning my clone of Xiaoyun Yang's repo

--- leetcode/1464-maximum-product-of-two-elements-in-an-array.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
- var maxProduct = function(nums) {
-  const sortedNums = nums.map(a=>a-1).sort((a,b) => b-a)
-  return sortedNums[0]*sortedNums[1]
-};
