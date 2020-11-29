ceeb3a272371e5ec3342a0735048fed9f64d474c
Paul Tegnazian <tegnazianpaul@gmail.com>
2020-11-29 16:16:53 -0500

    Update 1306 - simplification of dfs function

--- leetcode/1306-jump-game-iii.js
+++ leetcode/1306-jump-game-iii.js
@@ -1,17 +1,24 @@
+/**
+ * @param {number[]} arr
+ * @param {number} start
+ * @return {boolean}
+ */
 var canReach = function(arr, start) {
   if(!arr.includes(0)) return false
   const explored = new Set()
 
-  function traverse(i) {
+  function dfs(i) {
     if(arr[i] === 0) return true
     if(i >= arr.length || i < 0) return false
     if(explored.has(i)) return false
     explored.add(i)
     const indexL = i - arr[i]
     const indexR = i + arr[i]
-    const isZeroL = traverse(indexL)
-    const isZeroR = traverse(indexR)
+    const isZeroL = dfs(indexL)
+    const isZeroR = dfs(indexR)
     return isZeroL || isZeroR
   }
-  return traverse(start)
+  return dfs(start)
 };
+
+// can simplify: return dfs(i-arr[i]) || dfs(i+arr[i])
